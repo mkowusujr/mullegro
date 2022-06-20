756f6fde4bdb97d489c5cd5c607218ae981a6076
mathew owusu jr <mrfriiday@gmail.com>
2022-06-20 02:41:14 -0400

    feat: added code for auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  getUserDetails(): string | null {
+    if(localStorage.getItem('userData'))
+      return localStorage.getItem('userData');
+    else
+      return null;
+  }
+
+  setDataInLocalStorage(varName: string, data: string) {
+    localStorage.setItem(varName, data);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  clearStorage() {
+    localStorage.clear();
+  }
 }
