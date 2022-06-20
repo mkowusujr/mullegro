e1fd8b9218c630bcb649134263a8bd973fd91c95
mathew owusu jr <mrfriiday@gmail.com>
2022-06-20 02:53:55 -0400

    feat: added code for auth guard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _router: Router
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot, // route trying to acess
+    state: RouterStateSnapshot // current route
+    ): boolean {
+      if (this._authService.getToken()) // user is logged in
+        return true;
+        
+        // user isnt logged in
+        this._router.navigate(['/login']);
+        return false;
+    }
 }
