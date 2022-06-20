c1a662d0aeb4e3b79f1ec086a9bce233693617e6
mathew owusu jr <mrfriiday@gmail.com>
2022-06-20 02:33:17 -0400

    feat: finished code for api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = 'http://localhost:4000';
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getRequest(url: string): Observable<any> {
+    return this._http.get(`${this.baseUrl}${url}`)
+    .pipe(map(res => {
+      return res
+    }));
+  }
+
+  postRequest(url: string, body: {}): Observable<any> {
+    return this._http.post(`${this.baseUrl}${url}`, body)
+    .pipe(map(res => {
+      return res
+    }));
+  }
+
+  putRequest(url: string, body: {}): Observable<any> {
+    return this._http.put(`${this.baseUrl}${url}`, body)
+    .pipe(map(res => {
+      return res
+    }));
+  }
+
+  deleteRequest(url: string): Observable<any> {
+    return this._http.delete(`${this.baseUrl}${url}`)
+    .pipe(map(res => {
+      return res
+    }));
+  }
 }
