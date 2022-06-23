26b3ac83c0c2df3f63be75cf4e7048190a1d7273
mathew owusu jr <mrfriiday@gmail.com>
2022-06-23 02:21:08 -0400

    feat: added a home page component and more css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/auth/components/login/login.component';
 import { RegisterComponent } from './pages/auth/auth/components/register/register.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileComponent } from './pages/profile-page/profile.component';
 import { AuthGuardService } from './core/services/auth-guard.service';
+import { HomeComponent } from './pages/home-page/home.component';
 
 const routes: Routes = [
-	{path: 'login', component: LoginComponent} ,
+  {path: '', component: HomeComponent},
+	{path: 'login', component: LoginComponent},
 	{path: 'register', component: RegisterComponent},
   {path: 'profile', canActivate: [AuthGuardService], component: ProfileComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -7,14 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './pages/auth/auth/auth.module';
 import { NavbarComponent } from './shared/layouts/navbar/navbar.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileComponent } from './pages/profile-page/profile.component';
 import { AppendJWT } from './core/interceptors/appendJWT.interceptor';
+import { HomeComponent } from './pages/home-page/home.component';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProfileComponent,
+    HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
