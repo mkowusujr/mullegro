caf9c7a2a259d9cf5926bd1aa9aae5e90929fcfd
mathew owusu jr <mrfriiday@gmail.com>
2022-08-26 01:57:22 -0400

    Started work on auth pages

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,7 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  template: ` <p>login works!</p> `,
+  template: `
+    <div class="Login">
+      <h4>Login</h4>
+      <form> 
+        <input type="text" name="email_or_username" placeholder="Email or Username" ngModel required>
+        <input type="password" name="password" placeholder="Password" ngModel required>
+        <input type="submit" value="Login">
+    </form>
+    </div>
+  `,
   styles: []
 })
 export class LoginComponent implements OnInit {
--- src/app/pages/auth/signup/sign-up.component.ts
+++ src/app/pages/auth/signup/sign-up.component.ts
@@ -2,7 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
-  template: ` <p>sign-up works!</p> `,
+  template: `
+    <div class="Signup">
+      <h4>Sign Up</h4>
+      <form> 
+      <input type="text" name="firstname" placeholder="Enter First Name" required ngModel>
+        <input type="text" name="lastname" placeholder="Enter Last Name" required ngModel>
+        <input type="text" name="address" placeholder="Enter Adress" required ngModel>
+        <input type="email" name="email" placeholder="Enter Email" required ngModel>
+        <input type="text" name="username" placeholder="Enter Username" required ngModel>
+        <input type="password" name="password_1" placeholder="Enter Password" required ngModel>
+        <input type="password" name="password_2" placeholder="Re-enter Password" required ngModel>
+        <input type="submit" value="Signup">
+    </form>
+    </div>
+  `,
   styles: []
 })
 export class SignUpComponent implements OnInit {
