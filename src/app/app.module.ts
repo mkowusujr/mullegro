6240e650e99ee1a26e72730e469e734abae44dd9
mathew owusu jr <mrfriiday@gmail.com>
2022-08-25 21:19:28 -0400

    Setup footer and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 //import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { NavbarComponent } from './shared/layouts/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/layouts/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <footer>Author: Mathew Owusu Jr</footer>
+  `
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
