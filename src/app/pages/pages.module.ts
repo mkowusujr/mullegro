0d0ebfbcbb9fe1ea4a76aea421d144c625cbe91b
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-08-28 01:13:33 -0400

    Added vertical dividers

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { UserProfileComponent } from './user-profile/user-profile/user-profile.component';
-import { ThreeColumnDisplayComponent } from '../shared/layouts/tri-column-display/three-column-display.component';
+import { ThreeColumnDisplayComponent } from '../shared/layouts/three-column-display/three-column-display.component';
 
 @NgModule({
   imports: [CommonModule, PagesRoutingModule],
--- src/app/shared/layouts/three-column-display/three-column-display.component.ts
+++ src/app/shared/layouts/three-column-display/three-column-display.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'three-column-display',
   template: `
     <div class="tri-col-display">
       <ng-content select="[col1]"></ng-content>
+        <div class="vertical-divider"></div>
       <ng-content select="[col2]"></ng-content>
+        <div class="vertical-divider"div></div>
       <ng-content select="[col3]"></ng-content>
     </div>
   `
 })
-export class ThreeColumnDisplayComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {}
-
-}
+export class ThreeColumnDisplayComponent {}
