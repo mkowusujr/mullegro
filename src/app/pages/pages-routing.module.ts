4f81122719d69535d52bb3f6e3fe5e69f118b4b9
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-08-28 01:19:33 -0400

    Fixed file location

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AuthPagesModule } from './auth/auth-pages.module';
-import { UserProfileComponent } from './user-profile/user-profile/user-profile.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
-import { UserProfileComponent } from './user-profile/user-profile/user-profile.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ThreeColumnDisplayComponent } from '../shared/layouts/three-column-display/three-column-display.component';
 
 @NgModule({
