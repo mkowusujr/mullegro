def37caf88cb0668ee1158fb51f7b8a2b5c42d4b
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-08-27 23:22:07 -0400

    Added get token function

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -14,12 +14,16 @@ import { AuthStateService } from './auth-state.service';
 export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
+  private getJwtToken(): string | null {
+    return localStorage.getItem('jwt');
+  }
+  
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = localStorage.getItem('jwt');//this._authState.getJwtToken();
+    const token = this.getJwtToken();
     if (token)
       authReq = req.clone({
         headers: req.headers.set('Authorization', token)
