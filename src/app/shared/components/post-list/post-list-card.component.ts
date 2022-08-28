50bf6f585f64685490d3cc7b031de536ef19c939
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-08-28 02:27:08 -0400

    Added anchor tag

--- src/app/shared/components/post-list/post-list-card.component.ts
+++ src/app/shared/components/post-list/post-list-card.component.ts
@@ -4,11 +4,14 @@ import { Post } from 'src/app/core/interfaces/post';
 @Component({
   selector: 'post-list-card',
   template: `
+  <a [routerLink]="[(postRoute)]" routerLinkActive="active">
     <p>
       post-list-card works!
     </p>
+  </a>
   `
 })
 export class PostListCardComponent {
   @Input() post!: Post;
+  postRoute = `/post/${this.post.id}`;
 }
--- src/app/shared/components/post-list/post-list.module.ts
+++ src/app/shared/components/post-list/post-list.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostListComponent } from './post-list.component';
 import { PostListCardComponent } from './post-list-card.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [PostListComponent, PostListCardComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [PostListComponent]
 })
