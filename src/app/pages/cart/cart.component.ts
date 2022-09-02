66d2a3f7cc0803f18a33dfe9d568a733b70d78d9
mkowusujr <mrfriiday@gmail.com>
2022-09-02 10:01:05 -0400

    Added call to get all cart items

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/core/interfaces/post';
+import { CartService } from 'src/app/core/services/api/cart.service';
 
 @Component({
-  selector: 'app-cart',
+  selector: 'cart',
   template: `
-    <p>
-      cart works!
-    </p>
+    <ng-container *ngFor="let post of (cartItems$ | async)">
+      <div>
+        {{post.title}}
+        {{post.price}}
+      </div>
+    </ng-container>
   `,
   styles: [
   ]
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  cartItems$!: Observable<Post[]>;
+  constructor(private _cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartItems$ = this._cartService.getCartItems()
   }
 
 }
