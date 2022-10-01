bbfd2bffb2c8959b6b32156afe29cb3eda5b8c2e
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-10-01 02:48:16 -0400

    Added search feature

--- src/app/pages/posts/post-list-page/post-list-page.component.ts
+++ src/app/pages/posts/post-list-page/post-list-page.component.ts
@@ -7,6 +7,7 @@ import { PostService } from 'src/app/core/services/api/post.service';
 @Component({
   selector: 'post-list-page',
   template: `
+    <input type="text" placeholder="Search for Posts" (keyup)="onKey($event)" />
     <ng-container *ngIf="(posts$ | async)?.length != 0; else notFound">
       <ng-container *ngFor="let post of posts$ | async">
         <post-list-card [post]="post"></post-list-card>
@@ -15,19 +16,19 @@ import { PostService } from 'src/app/core/services/api/post.service';
     <ng-template #notFound>
       <h1>No {{ filter }} Posts Found At The Moment</h1>
     </ng-template>
-  `,
-  styles: []
+  `
 })
 export class PostListPageComponent implements OnInit {
   posts$!: Observable<Post[]>;
   filter!: string | undefined;
+  searchQuery = '';
 
   constructor(
     private _postService: PostService,
     private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {
+  initalFetchPosts() {
     this.route.queryParams.pipe(take(1)).subscribe(params => {
       if (params['category']) {
         this.posts$ = this._postService.getFilteredPosts(
@@ -45,5 +46,18 @@ export class PostListPageComponent implements OnInit {
         this.posts$ = this._postService.getAllPosts();
       }
     });
+  }
+
+  ngOnInit(): void {
+    this.initalFetchPosts();
+  }
+
+  onKey(event: any) {
+    this.searchQuery = event.target.value;
+    if (this.searchQuery === '') {
+      this.posts$ = this._postService.getAllPosts();
+    } else {
+      this.posts$ = this._postService.findPostWithSearchQuery(this.searchQuery);
+    }
   }
 }
