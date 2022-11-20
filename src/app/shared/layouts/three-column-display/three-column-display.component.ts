ba965ccca737eabbecac189d7b6c8053bbab2c97
mkowusujr <mrfriiday@gmail.com>
2022-11-20 03:22:54 -0500

    Updates to the three col display

--- src/app/shared/layouts/three-column-display/three-column-display.component.ts
+++ src/app/shared/layouts/three-column-display/three-column-display.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <ng-content select="[col1]"></ng-content>
     <ng-content select="[col2]"></ng-content>
-    <div class="vertical-divider"></div>
+    <!-- <div class="vertical-divider"></div> -->
     <ng-content select="[col3]"></ng-content>
   `
 })
